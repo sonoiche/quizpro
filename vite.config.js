c0b5223e996cfa48b35ed1fe85ac6351a87fff56
Jelson Lanto <jelsonlanto@Jelsons-iMac.local>
2024-10-01 11:24:56 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,10 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/assets/libs/bootstrap/css/bootstrap.min.css',
+                'resources/assets/css/styles.css',
+                'resources/assets/css/icons.css',
+                'resources/assets/css/custom.css'
             ],
             refresh: true,
         }),
